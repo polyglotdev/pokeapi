edd4eeb4da109c7955f85478034f21390a188fec
Dom Hallan <dom@domhallan.com>
2023-07-10 16:32:57 -0500

    Refactor pokeapi.go to include a new method GetPokemon that retrieves a Pokemon by name or ID.
    - Added struct Pokemon with fields Name and ID.
    - Added method GetPokemon to Client, which makes an HTTP GET request to retrieve a specific Pokemon.
    - Updated NewClient function to initialize the baseURL.

--- pokeapi.go
+++ pokeapi.go
@@ -1,11 +1,21 @@
 package pokeapi
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // Client is the http client for the pokeapi -- for testing purposes only
 type Client struct {
 	httpClient *http.Client
 	baseURL    string
+}
+
+// Pokemon is the struct for a pokemon
+type Pokemon struct {
+	Name string `json:"name"`
+	ID   int    `json:"id"`
 }
 
 // NewClient returns a new pokeapi client
@@ -14,4 +24,24 @@ func NewClient() *Client {
 		httpClient: http.DefaultClient,
 		baseURL:    "https://pokeapi.co/api/v2/",
 	}
+}
+
+// GetPokemon takes in a pokemon name or id and returns a pokemon
+func (c *Client) GetPokemon(idOrName string) (*Pokemon, error) {
+	resp, err := c.httpClient.Get(c.baseURL + "pokemon/" + idOrName + "/")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get pokemon: %s", resp.Status)
+	}
+
+	var pokemon Pokemon
+	if err := json.NewDecoder(resp.Body).Decode(&pokemon); err != nil {
+		return nil, err
+	}
+
+	return &pokemon, nil
 }
